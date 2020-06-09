5f229bd8b0a786711b426f75b3b41b5b10d7bf7d
Maaz <maazazher28@gmail.com>
2020-06-10 00:29:02 +0500

    React State - Bootcamp 2020 - Assignment 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [isLit, setLit] = useState('Light');
+  let [tempr, setTemp] = useState(72);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${isLit}`}>
+      <h1>React State - Bootcamp 2020 - Assignment 4</h1>
+      <br />
+      <h3>Dark Mode / Light Mode</h3>
+      <button onClick={() => {
+        setLit('Dark');
+        console.log(isLit);
+      }
+      }>ON</button>
+      <button onClick={() => {
+        setLit('Light');
+        console.log(isLit);
+      }
+      }
+      >OFF</button>
+      <br />
+      <hr />
+      <br />
+      <h3>Temperature</h3>
+      <p>{`Current Temperature: ${tempr}°F`}</p>
+      <button onClick={() => {
+        setTemp(tempr + 1);
+      }
+      }>+</button>
+      <button onClick={() => {
+        setTemp(tempr - 1);
+      }
+      }
+      >-</button>
     </div>
   );
 }
